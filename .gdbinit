64f3a301fdb9b0ae8182b0777f278b513c86762b
Leo Nunner <leo@leonunner.com>
2020-05-12 21:18:20 +0200

    I'm so happy aslkdnajipsdfnpjiasdfnipjasfdnp

--- .gdbinit
+++ .gdbinit
@@ -2,6 +2,10 @@ define print_array
     print *($arg0)@(sizeof($arg0)/ sizeof($arg0[0]))
 end
 
+define dump_page
+	x/4096 $arg0
+end
+
 layout asm
 layout reg
 layout split 
@@ -10,3 +14,7 @@ target remote localhost:26000
 file img.elf
 b init_paging
 b handle_supervisor_trap
+
+b map_object if enabled 
+b find if enabled 
+b map_range if enabled 
--- kernel/src/memory/paging.c
+++ kernel/src/memory/paging.c
@@ -34,18 +34,23 @@ pt_t *current_table;
  * Paging code
  */
 
-extern void
-fence(uint64_t addr);
 
 void
 switch_table(pt_t *t);
 
+/**
+ * What this does is a little weird. I have one page where I map 
+ * all the things to I need while allocating new pages. This changes
+ * the pointer of the original thingy and restores it when it's done.
+ */
 void
-set_placeholder(uint64_t paddr)
+map_object(uint64_t addr)
 {
-	placement->ppn = (paddr >> 12);
+	if(!enabled) return;
+
+	placement->ppn = (addr >> 12);
 	placement->flags = PTE_W | PTE_R | PTE_V;
-	fence(paddr);
+	switch_table(current_table);
 }
 
 pte_t *
@@ -54,22 +59,27 @@ find(pt_t *pagetable, uint64_t va, bool create)
 	pte_t *page;
 	pt_t  *current = pagetable;
 
-	for(int i = 2; i > 0; i--) {
+	if(enabled) {
+		map_object((uint64_t) pagetable);
+		current = (pt_t *) placement_addr;
+	}
+
+	// May god forgive me, for I have sinned.
+
+	for(size_t i = 2; i > 0; i--) {
 		page = &current->pages[VPN(va, i)];
-		if(enabled) set_placeholder((uint64_t) page);
+
 		if(page == 0 || !(page->flags & PTE_V)) {
 			if(!create) return 0;
+			
+			uint64_t addr = (uint64_t) kmalloc_a(sizeof(pt_t));
 
-			pt_t *table = (pt_t *) kmalloc_a(sizeof(pt_t));
-			if(enabled) set_placeholder((uint64_t) table);
-			memset(table, 0, sizeof(pt_t)); 
-			if(enabled) set_placeholder((uint64_t) page);
-			page->ppn = (uint64_t) table >> 12;
+			page->ppn = addr >> 12;
 			page->flags |= PTE_V;
 		}
+		map_object(PA(page));
 
-		if(enabled) current = (pt_t *) placement_addr;
-		else 		current = (pt_t *) PA(page);
+		if(!enabled) current = (pt_t *) PA(page);
 	}
 
 	return &current->pages[VPN(va, 0)];
@@ -78,6 +88,8 @@ find(pt_t *pagetable, uint64_t va, bool create)
 void
 map_range(uint64_t vaddr, uint64_t paddr, uint64_t size, uint64_t flags)
 {
+	pte_t *page;
+
 	vaddr &= ~(0x1FFF);
 	for(size_t i = 0; i <= size; i += PAGE_SIZE)
 	{
@@ -85,13 +97,15 @@ map_range(uint64_t vaddr, uint64_t paddr, uint64_t size, uint64_t flags)
 		alloc_frame(paddr);
 
 		uint64_t pos = vaddr + i;
-		pte_t *page = find(current_table, pos, TRUE);
+		page = find(current_table, pos, TRUE);
+
 		page->flags = flags;
 		page->ppn = (uint64_t) ((paddr + i) >> 12);
 		page->flags |= PTE_V;
 	}
 
-	__asm__("sfence.vma");
+	// To flush.
+	if(enabled) switch_table(current_table);
 }
 
 void
@@ -115,9 +129,11 @@ switch_table(pt_t *table)
 	if(table == 0)
 		return;
 
+	csr_clear(satp, (uint64_t) 8 << 60);
+	__asm__("sfence.vma");
 	current_table = table;
 	csr_set(satp, (uint64_t) table >> 12);
-	__asm__("sfence.vma");
+	csr_read_set(satp, (uint64_t) 8 << 60);
 }
 
 void
@@ -137,11 +153,10 @@ init_paging()
 
 	map_range((uint64_t) &__kernel_start, (uint64_t) &__kernel_start,  (uint64_t ) &__kernel_end - (uint64_t) &__kernel_start, PTE_W | PTE_R);
 	map_range((uint64_t) placement, (uint64_t) placement, PAGE_SIZE, PTE_W | PTE_R);
-	//map_range((uint64_t) &__uart, (uint64_t) &__uart, 0x100, PTE_W | PTE_R);
+	map_range((uint64_t) &__uart, (uint64_t) &__uart, 0x100, PTE_W | PTE_R);
 
 	setup_heap();
 	switch_table(root_table);
-	csr_read_set(satp, (uint64_t) 8 << 60);
 	enabled = TRUE;	
 
 	map_range((uint64_t) &__uart, (uint64_t) &__uart, 0x100, PTE_W | PTE_R);
